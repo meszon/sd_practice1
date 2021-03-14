11f25430e7e3e7c2a36a41d06d8d322677f7978a
Raul Mesa <meszon@gmail.com>
2021-03-14 18:42:58 +0100

    worker create/delete

--- sd_master.py
+++ sd_master.py
@@ -1,6 +1,12 @@
 from xmlrpc.server import SimpleXMLRPCServer
+from multiprocessing import Process
 import logging
+import redis
 import os
+
+# Workers
+workers = {}
+worker_id = 0
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -9,25 +15,52 @@ server = SimpleXMLRPCServer(
     ('localhost', 9000),
     logRequests=True,
 )
-
+    
+# Create worker
 def createWorker():
-    return "Create worker"
+    global workers
+    global worker_id
+
+    proc = Process(target=printProcess, args=(worker_id,))
+    proc.start()
+    workers[worker_id] = proc
+    worker_id += 1
+
+    #return "Create worker"
+    return str(proc)
 
 server.register_function(createWorker)
 
+# Print proc example
+def printProcess(id):
+    global workers
+    print(str(id))
+
+# Delete worker
 def deleteWorker():
+    global workers
+    global worker_id
+
+    worker_id -=1
+    proc = workers[worker_id]
+    proc.kill()
+    print(str(worker_id))
+
     return "Delete worker"
 
 server.register_function(deleteWorker)
 
+# List worker
 def listWorker():
     return "List worker"
 
 server.register_function(listWorker)
 
+# Word Count
 def runWordCount():
     return "Run word count"
 
+# Count Words
 server.register_function(runWordCount)
 
 def runCountWords():
