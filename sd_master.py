b9db2ac41499c487c2e980e9863feb630b79af18
Raul Mesa <meszon@gmail.com>
2021-03-17 20:08:01 +0100

    workers: get job redis

--- sd_master.py
+++ sd_master.py
@@ -2,6 +2,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 from multiprocessing import Process
 import functions
 import logging
+import time
 import redis
 import os
 import json
@@ -25,6 +26,8 @@ server = SimpleXMLRPCServer(
 
 # Redis database connection
 r = redis.Redis(host='localhost', port=6379, db=0)
+r.rpush('queue:tasks', "")
+r.rpush('queue:results', "")
     
 # Create worker
 def createWorker():
@@ -44,8 +47,15 @@ server.register_function(createWorker)
 # Start worker
 def startWorker(id):
     global workers
-
-    print(str(id))
+    while True:
+        queue_list = r.lrange('queue:tasks', 1, -1)
+        if queue_list:
+            #task = r.lindex('queue:tasks', 1)
+            #r.lrem('queue:tasks', 1, task)
+            #task = str(task).replace("b", "").replace("\'", "")
+            #print(task)
+            print(list(queue_list))
+            print(str(id))
 
 # Delete worker
 def deleteWorker():
@@ -70,41 +80,36 @@ server.register_function(listWorker)
 # Name files adaptation
 def nameFilesArray(nameFiles, typeTask):
     global taskNumber
-    r.rpush('queue:tasks', json.dumps(""))
     nameFiles = nameFiles.replace("[", "")
     nameFiles = nameFiles.replace("]", "")
-    final_body=""
     arrayFiles = nameFiles.split(",")
+    result_tasks = ""
+
     for i in arrayFiles:
         print("i: "+ str(i))
         task='task' + str(taskNumber)
+        result_tasks = result_tasks + task + ";"
         body=task + ';' + str(typeTask) + ';P;' + str(i)
-        r.set (str(task), str(body))
-        #r.rpushx('queue:tasks', json.dumps(final_body))
-        final_body = final_body + "-" + body
+        r.rpushx('queue:tasks', body)
         taskNumber+=1
     
-    r.rpushx('queue:tasks', json.dumps(final_body))
-    #task='task' + str(taskNumber)
-    #body=task + ';' + str(typeTask) + ';R'
-    #r.set (str(task), str(body))
-    #taskNumber+=1
-    #r.rpush('queue:results', json.dumps(task))
-    result = r.blpop(['queue:tasks'], 3)
-    print(str(result))
+    task='task' + str(taskNumber)
+    taskNumber+=1
+    body=task + ';' + str(typeTask) + ';P;'
+    r.rpushx('queue:tasks', body)
+    body_tasks = task + ":" + result_tasks
+    r.rpushx('queue:results', body_tasks)
  
- 
-
 # Word Count
 def runWordCount(nameFiles):
-    nameFilesArray(nameFiles, "W")
+    nameFilesArray(nameFiles, "Wordcount")
     return str(nameFiles)
 
 server.register_function(runWordCount)
 
 # Count Words
 def runCountWords(nameFiles):
-    nameFilesArray(nameFiles, "C")
+    nameFilesArray(nameFiles, "Countwords")
     return str(nameFiles)
 
 server.register_function(runCountWords)
